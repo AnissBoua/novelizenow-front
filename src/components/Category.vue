4d5afd915b78741cc10414dfb92ad4be5f49751b
Anisse <anissbouainbi@hotmail.it>
2023-03-05 21:03:51 +0100

    comp category

---
+++ src/components/Category.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        category: {
+            type: String,
+            default: 'Romance'
+        },
+        btnStyle: {
+            type: Boolean,
+            default: false
+        },
+        color: {
+            type: String,
+            default: 'text-novelize-primary'
+        }
+    }
+}
+</script>
+<template>
+    <div>
+        <div v-if="btnStyle" :class="'w-max ' + color + ' rounded-full text-center px-4'">
+            <p class="font-medium text-white capitalize">{{ category }}</p>
+        </div>
+        <div v-else>
+            <p :class="'text-base ' + color + ' capitalize '">{{ category }}</p>
+        </div>
+    </div>
+</template>
