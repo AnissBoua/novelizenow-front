cef1fd21d3f2f57ab972fc39fd11c9cf57ea3890
Anisse <anissbouainbi@hotmail.it>
2023-03-05 15:56:12 +0100

    config tailwind

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.scss'
+import './assets/style.css'
 
 import CKEditor from '@ckeditor/ckeditor5-vue';
 
