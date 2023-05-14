47c020ee598f9d9c58d18fe1e849da704f7f1571
Anisse <anissbouainbi@hotmail.it>
2023-05-14 19:46:58 +0200

    dernier chapitre dynamic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,64 +8,40 @@ import TopAuthor from '../components/home/TopAuthor.vue';
 import Select from '../components/inputs/Select.vue';
 import TopNovel from '../components/home/TopNovel.vue';
 import NewRelease from '../components/home/NewRelease.vue';
+
+import axios from 'axios';
+import { ref } from 'vue';
+
+const carousel = ref([]);
+const chapters = ref([]);
+const categories = ref([]);
+const newNovels = ref([]);
+
+axios.get('/home').then(res => {
+  console.log(res.data);
+  carousel.value = res.data.carousel;
+  chapters.value = res.data.chapters;
+  categories.value = res.data.categories;
+  newNovels.value = res.data.newNovels;
+})
+
+
 </script>
 
 <template>
   <main>
     <Carousel/>
     <div class="flex justify-between px-8">
-      <div class="w-8/12">
-        <div class="my-8">
-          <div class="flex">
-            <Category 
-            class="mr-4"
-            color="white"
-            />
-            <Category 
-            class="mr-4"
-            category="Adventure"
-            :active="true"
-            />
-            <Category 
-            class="mr-4"
-            category="Horror"
-            color="white"
-            />
-            <Category 
-            class="mr-4"
-            category="Action"
-            color="white"
-            />
-          </div>
-          <div class="flex overflow-x-scroll py-4 scrollbar">
-            <div class="min-w-[40em] mr-4">
-              <Novel />
-            </div>
-            <div class="min-w-[40em] mr-4">
-              <Novel />
-            </div>
-            <div class="min-w-[40em] mr-4">
-              <Novel />
-            </div>
-          </div>
-        </div>
+      <div class="w-full mx-auto">
         <div class="bg-neutral-800 rounded-lg py-2 px-4 my-8">
           <h3 class="text-xl font-semibold my-2">Last chapters</h3>
-          <div class="my-4">
-            <div class="my-4" v-for="i in 8" :key="i">
-              <Chapter />
+          <div class="grid grid-cols-2 gap-4 my-4">
+            <div class="" v-for="(chapter, i) in chapters" :key="i">
+              <Chapter :title="chapter.title" :novel="chapter.novel" :author="chapter.novel.author" />
             </div>
           </div>
         </div>
       </div>
-      <div class="w-4/12 my-8 mx-8">
-        <div class="h-max bg-neutral-800 rounded-lg py-2 px-4 ">
-          <TopAuthor/>
-        </div>
-        <div class="h-max bg-neutral-800 rounded-lg py-2 px-4 my-4">
-          <TopNovel />
-        </div>
-      </div>      
     </div>
     <div class="px-8">
       <NewRelease />
